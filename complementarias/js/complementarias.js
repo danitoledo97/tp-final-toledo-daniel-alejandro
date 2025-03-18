8a418752f3a671b36aee5675ff6cc65d990fa702
Daniel Alejandro Toledo <danielalejandrotoledo97@gmail.com>
2025-03-18 11:15:24 -0300

    Actualización
    Se terminó de definir la estructura de la barra de navegación junto con el contenido y se ha implementado el framework css Bootstrap para los estilos. Igualmente aún se esperan posibles actualizaciones mínimas con respecto al contenido y la posibilidad de reemplazar a Bootstrap por el framework Tailwind CSS.

---
+++ complementarias/js/complementarias.js
@@ -0,0 +1,42 @@
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      else
+      {
+        alert('¡Datos Validados!')
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+window.onload = function () {
+
+    var form = document.getElementById("form1");
+
+    // create the pristine instance
+    var pristine = new Pristine(form);
+
+    form.addEventListener('submit', function (e) {
+       e.preventDefault();
+       
+       // check if the form is valid
+       var valid = pristine.validate(); // returns true or false
+
+    });
+};
+
+
